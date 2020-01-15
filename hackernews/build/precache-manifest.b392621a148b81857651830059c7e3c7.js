bdbe726fa8583167cfb085911aec5e9293f6c2d8
catmother <catm0th3r@gmail.com>
2020-01-15 16:06:51 +0545

    Adding hackernews directory and progress thus far

---
+++ hackernews/build/precache-manifest.b392621a148b81857651830059c7e3c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b2c71de18bd73035ece500a8006383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c5a26414b213e0e7d76",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b31d9f54404d87837c4f",
+    "url": "/static/js/2.6949454a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6949454a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c5a26414b213e0e7d76",
+    "url": "/static/js/main.ed1b146c.chunk.js"
+  },
+  {
+    "revision": "1089a7db97561975b47b",
+    "url": "/static/js/runtime-main.8843425f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
